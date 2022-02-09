cf67e219b4f6f2a5650749491552f88d30f4ae18
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-02-09 09:16:47 +0530

    C++ Code snippet

---
+++ first.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MP make_pair
+#define PB push_back
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef map<int,int> MPII;
+typedef unordered_set<int> USI;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(long long t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+/*---------------Template Ends-------------------*/
+
+
+
+//solution
+void solve(){
+    
+}
+
+
+
+//Main Block
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+
+    int t;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+}
+
